5fe578df464d04a1a00de157afc1c1aad32d7b03
luigi112 <riosmunar_luigi@hotmail.com>
2022-06-09 15:08:17 -0500

    Version 2

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -18,6 +18,10 @@ public class Cv_ejercicio {
         // TODO code application logic here
         System.out.println("Version 0");
         System.out.println("Version 1");
+        System.out.println("Version 2");
+        System.out.println("Version 3");
+        
+        
     }
     
 }
